adadeb5a763390b39cc9b6357f332e0c97cd7838
Arnas <arnas@Arnass-MacBook-Air.local>
2025-05-02 22:01:19 +0300

    Something is happening with data adding to db

---
+++ lets_do_smth_with_data.py
@@ -0,0 +1,61 @@
+import math
+import time
+
+from concurrent.futures import ThreadPoolExecutor
+
+import pandas as pd
+from pymongo import MongoClient
+from tqdm import tqdm
+
+from logger_config import get_logger
+
+
+CSV_PATH = "/Users/arnas/big_data_3/big_data_3/ais_dataset/aisdk-2025-04-20/aisdk-2025-04-20.csv"
+CHUNK_SIZE = 10000
+NUM_THREADS = 4
+
+logger = get_logger("task.log")
+
+def insert_chunk(chunk_df):
+    client = MongoClient("mongodb://localhost:27017")
+    db = client["ais"]
+    collection = db["records"]
+
+    records = chunk_df.to_dict(orient="records")
+    collection.insert_many(records)
+    client.close()
+
+def main():
+    logger.info("Loading and cleaning data")
+    df = pd.read_csv(CSV_PATH)
+
+    logger.info(f"Total rows: {len(df)}")
+    logger.info(f"Doing cleaning")
+
+    df.rename(columns={"# Timestamp": "Timestamp"}, inplace=True)
+    df = df[["MMSI", "Timestamp", "Latitude", "Longitude", "SOG", "COG"]]
+    df.dropna(subset=["MMSI", "Timestamp", "Latitude", "Longitude"])
+
+    total_rows = len(df)
+    logger.info(f"After cleaning left rows: {total_rows}")
+
+    chunks = math.ceil(total_rows / CHUNK_SIZE)
+    with ThreadPoolExecutor(max_workers=NUM_THREADS) as ex:
+        futures = []
+        for i in tqdm(range(chunks), desc="Inserting chunks"):
+            start = i * CHUNK_SIZE
+            end = min(start + CHUNK_SIZE, total_rows)
+            chunk_df = df.iloc[start:end]
+            futures.append(ex.submit(insert_chunk, chunk_df))
+
+        for f in futures:
+            f.result()
+
+    print("Data was inserted succesfully.")
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    total_time = time.time() - start_time
+    logger.info(f"Total execution time: {total_time:.2f} seconds.")
+    print(f"Total execution time: {total_time:.2f} seconds.")
